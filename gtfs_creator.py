dae7abd96ddeb0c04a5debb98a7aaa1fa27331e9
Wim Michiels <wim.michiels76@gmail.com>
2017-02-08 17:41:51 +0100

    Add files via upload

--- gtfs_creator.py
+++ gtfs_creator.py
@@ -1,21 +1,29 @@
-#import json
+import json
 
 # Read in init data
 
 
-#parsed_init = json.loads(open("init.json", 'rb'))
-#geodata_file = parsed_init["geodata_file"]
-#parsed_geodata = json.loads(open(geodata_file, 'rb'))
+parsed_init = json.loads(open('init.json','r').read())
+geodata_file = parsed_init['geodata_file']
+parsed_geodata = json.loads(open(geodata_file,'r').read())
 
 # read data from geodata_file
-
-#test_josm = parsed_json['generator']
-
+output="stop_id,stop_name,stop_lat,stop_lon"+"\n"
+stop_id=1
+test_josm = parsed_geodata['generator']
+for item in parsed_geodata['features']:
+    if item.get("properties"):
+        output+=str(stop_id)+","
+        output+=str(item.get('properties').get("name"))+","
+        output+=str(item.get('geometry').get("coordinates")[1])+","
+        output+=str(item.get('geometry').get("coordinates")[0])
+        output+="\n"
+        stop_id+=1
+        
 #create file "Stop-times"
 
-output="test_josm" 
-    
+   
 # opens an geoJSON file to write the output to
-outFileHandle = open("output.geojson", "w")
+outFileHandle = open("output.geojson",'w')
 outFileHandle.write(output)
 outFileHandle.close()
